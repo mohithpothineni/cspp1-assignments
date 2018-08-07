44464ad1a95939f7d6ca3a3655bd6ae8a8682644
mohithpothineni <mohith.pothineni@msitprogram.net>
2018-08-07 11:51:16 +0530

    power using recursion

--- m8/power using recursion/power_recr.py
+++ m8/power using recursion/power_recr.py
@@ -1,23 +1,27 @@
+''' power using recursion '''
 # Exercise: PowerRecr
-# Write a Python function, recurPower(base, exp), that takes in two numbers and returns the base^(exp) of given base and exp.
+# Write a Python function, recurPower(base, exp),
+#that takes in two numbers and returns the base^(exp) of given base and exp.
 
 # This function takes in two numbers and returns one number.
 
 
-def recurPower(base, exp):
+def recur_power(base, exp):
     '''
     base: int or float.
     exp: int >= 0
- 
     returns: int or float, base^exp
     '''
     # Your code here
-    
+    if exp == 0:
+        return 1
+    return base * recur_power(base, exp-1)
 
 def main():
+    '''takes input from user and calls power func '''
     data = input()
     data = data.split()
-    print(recurPower(float(data[0]),int(data[1])))   
+    print(recur_power(float(data[0]), int(data[1])))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
