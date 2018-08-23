4ffcb2b61bde3a88154657cf478cb7b74081b16f
mohithpothineni <mohith.pothineni@msitprogram.net>
2018-08-23 12:26:06 +0530

    " -> 5 of 8 passed. pylint: 10.00/10 "

--- ProblemSet4/CodeCampMatrixOperations/matrix_operations.py
+++ ProblemSet4/CodeCampMatrixOperations/matrix_operations.py
@@ -1,4 +1,5 @@
-def mult_matrix(m1, m2):
+''' matrix operations'''
+def mult_matrix(m1_, m2_):
     '''
         check if the matrix1 columns = matrix2 rows
         mult the matrices and return the result matrix
@@ -6,34 +7,36 @@ def mult_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    m1_r_len = len(m1)
-    m1_c_len = len(m1[0])
+    if m1_ is None or m2_ is None:
+        return None
+    m1__r_len = len(m1_)
+    m1__c_len = len(m1_[0])
 
-    m2_r_len = len(m2)
-    m2_c_len = len(m2[0])
-    
-    if m1_c_len != m2_r_len:
-        print("cant multiply")
-        return
+    m2__r_len = len(m2_)
+    m2__c_len = len(m2_[0])
+
+    if m1__c_len != m2__r_len:
+        print("Error: Matrix shapes invalid for multNone")
+        return None
     res = []
-    
-    for i in range(m1_r_len):
+
+    for i in range(m1__r_len):
         tmp_ = []
-        for j in range(m2_c_len):
+        for j in range(m2__c_len):
             tmp_.append(0)
         res.append(tmp_)
-        
-    
-    for i in range(m1_r_len):
-        for j in range(m2_c_len):
+
+
+    for i in range(m1__r_len):
+        for j in range(m2__c_len):
             tmp_ = 0
-            for k in range(m1_c_len):
-                tmp_ += m1[i][k]*m2[k][j]
+            for k in range(m1__c_len):
+                tmp_ += m1_[i][k]*m2_[k][j]
             res[i][j] = tmp_
     return res
-    
-    
-def add_matrix(m1, m2):
+
+
+def add_matrix(m1_, m2_):
     '''
         check if the matrix shapes are similar
         add the matrices and return the result matrix
@@ -41,28 +44,30 @@ def add_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    m1_r_len = len(m1)
-    m1_c_len = len(m1[0])
-    
-    m2_r_len = len(m2)
-    m2_c_len = len(m2[0])
-    
-    if m1_r_len != m2_r_len or m1_c_len != m2_c_len:
-        print("cant be added ")
-        return 
-    
+    if m1_ is None or m2_ is None:
+        return None
+    m1__r_len = len(m1_)
+    m1__c_len = len(m1_[0])
+
+    m2__r_len = len(m2_)
+    m2__c_len = len(m2_[0])
+
+    if m1__r_len != m2__r_len or m1__c_len != m2__c_len:
+        print("Error: Matrix shapes invalid for additionNone")
+        return None
+
     res = []
-    for i in range(m1_r_len):
+    for i in range(m1__r_len):
         tmp_ = []
-        for j in range(m1_c_len):
-            tmp_.append(m1[i][j] + m2[i][j])
+        for j in range(m1__c_len):
+            tmp_.append(m1_[i][j] + m2_[i][j])
         res.append(tmp_)
     return res
 
-            
-        
-    
-    
+
+
+
+
 def read_matrix():
     '''
         read the matrix dimensions from input
@@ -72,31 +77,40 @@ x`        create a list of lists and read the numbers into it
         error message should be "Error: Invalid input for the matrix"
     '''
     dim_ = input()
-    
-    row_,column_ = dim_.split(",")
-    
+
+    row_, column_ = dim_.split(",")
+
     matr_ = []
-    
+
     for i in range(int(row_)):
         row_inp = input()
-        matr_.append([int(e) for e in row_inp.split(" ")])
-    
-    return matr_ 
-   
-    
+        k = [int(e) for e in row_inp.split(" ")]
+        if len(k) != int(column_):
+            print("Error: Invalid input for the matrix")
+            return None
+        matr_.append(k)
+        i += 1
+    return matr_
+
+
 
 def main():
+    '''takes user input and calls the appropriate funcs '''
     # read matrix 1
-    m1 = read_matrix()
+    m1_ = read_matrix()
     # read matrix 2
-    m2 = read_matrix()
+    m2_ = read_matrix()
     # add matrix 1 and matrix 2
-    print(add_matrix(m1, m2))
+    k = add_matrix(m1_, m2_)
+    if k is not None:
+        print(k)
     # multiply matrix 1 and matrix 2
-    print(mult_matrix(m1, m2))
-    
-    
-    
+    k = mult_matrix(m1_, m2_)
+    if k is not None:
+        print(k)
+
+
+
 
 if __name__ == '__main__':
     main()
